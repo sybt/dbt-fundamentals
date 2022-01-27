9ff4ee594f146a04a1c66127f7f8460a67c083fd
blair.tang@aginic.com <blair.tang@aginic.com>
2022-01-27 12:16:21 +0000

    testing

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,7 +1,7 @@
 select id as payment_id
 , orderid as order_id
 , paymentmethod as payment_method
-, status
+, status as payment_status
 , amount/100 as dollar_amount
 , created as created_at
 from {{ source('stripe','payments') }}
