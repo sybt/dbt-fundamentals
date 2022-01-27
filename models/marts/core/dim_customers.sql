38f9eb22faea13c4d6b691e337b49b0fc1603cd2
blair.tang@aginic.com <blair.tang@aginic.com>
2022-01-27 02:52:14 +0000

    exercise 1

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -8,12 +8,19 @@ orders as (
     select * from {{ ref('stg_orders')}}
 ),
 
+payments as (
+    select * from {{ ref('stg_payments')}}
+),
+
 customer_orders as (
     select customer_id
-    , min(order_date) as first_order_date
-    , max(order_date) as most_recent_order_date
-    , count(order_id) as number_of_orders
-    from orders
+    , min(o.order_date) as first_order_date
+    , max(o.order_date) as most_recent_order_date
+    , count(o.order_id) as number_of_orders
+    , sum(p.dollar_amount) as lifetime_value
+    from orders o
+    left join payments p on o.order_id = p.order_id
+    where p.status = 'success'
     group by customer_id
 ),
 
@@ -24,6 +31,7 @@ final_dim_customers as (
     , co.first_order_date
     , co.most_recent_order_date 
     , coalesce(co.number_of_orders,0) as number_of_orders
+    , coalesce(co.lifetime_value,0) as lifetime_value
     from customers c 
     left join customer_orders co on c.customer_id = co.customer_id
 )
